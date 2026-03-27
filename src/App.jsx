70f3b6e2ea93984e81ade968b11ad9da767da953
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-03-28 00:49:14 +0600

    initialized git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Countries from "./components/countries/countries";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>World On The Go...</h1>
+      </section>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
